b362add020e4c43b9216fe08b553048bc3dcff2f
William <lichengqi0805@gmail.com>
2019-09-08 19:26:45 +0800

    update formulae

--- brdffunc.py
+++ brdffunc.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 
 #import matplotlib.pyplot as plt
@@ -15,10 +14,15 @@ def Phong_D(roughness, theta):
 
 
 # Beckmann D分布，from cook paper, ggx paper
-def Beckmann_D(roughness, theta):
-    cos_theta = np.cos(theta)
-    tan_theta = np.tan(theta)
+def Beckmann_D(roughness, delta):
+    cos_theta = np.cos(delta)
+    tan_theta = np.tan(delta)
     return np.exp(-np.power(tan_theta / roughness, 2)) / (np.pi * roughness * roughness * np.power(cos_theta, 4))
+
+def CT_D(delta):
+    cos_theta = np.cos(delta)
+    tan_theta = np.tan(delta)
+    return np.exp(-np.power(tan_theta, 2)) / (np.pi * np.power(cos_theta, 4))
 
 def TrowbridgeReitz_D(roughness, theta):
     c = 1 / np.pi / roughness / roughness
@@ -28,9 +32,13 @@ def TrowbridgeReitz_D(roughness, theta):
 def GGX_D(roughness, theta):
     cos_theta = np.cos(theta)
     tan_theta = np.tan(theta)
-
     return roughness * roughness / (np.pi * np.power(cos_theta, 4) * np.power(roughness * roughness + np.power(tan_theta, 2), 2))
 
-def CT_D(delta, roughness):
-    #roughness = alpha = m^2
-    return math.exp(-math.tan())
+def G_shadow(alpha, delta, theta_r, theta_i):
+    first = 2*np.cos(delta)*np.cos(theta_r)/np.cos(alpha)
+    second = 2*np.cos(delta)*np.cos(theta_i)/np.cos(alpha)
+    minimum = min(1, first, second)
+    return minimum
+
+def Fresnel(C_spec, alpha):
+    return C_spec+(1-C_spec)*np.power(1-np.cos(alpha), 5)
